f338c7020be325fb9c4d120fa4b0dd09fed601a8
Abraham Bueno <abueno@localhost.localdomain>
2018-05-28 17:44:36 -0500

    Added the mongodb and the routes as a template.

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 const cool = require('cool-ascii-faces')
 const express = require('express')
-const path = require('path')
+const path = require('path');
+const helmet = require('helmet');
+const cors = require('cors');
+
 const PORT = process.env.PORT || 5000
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+const server = express();
+const uri = 'mongodb://heroku_m7f5h0h6:pk20t1sracjbphn4kiteubek8u@ds137740.mlab.com:37740/heroku_m7f5h0h6';
+
+var options = {
+  "server": {
+    "socketOptions": {
+      "keepAlive": 300000,
+      "connectTimeoutMS": 30000
+    }
+  },
+  "replset": {
+    "socketOptions": {
+      "keepAlive": 300000,
+      "connectTimeoutMS": 30000
+    }
+  }
+}
+
+mongoose.connect(uri, options);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+server.use(helmet());
+server.unsubscribe(cors());
+server.use(express.json());
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req,res) => res.send(cool()))
+  .get('/test', function (req, res) {
+    res.status(200).json({ api: 'running' });
+  })
+  .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => {
     let result = ''
     const times = process.env.TIMES || 5
@@ -17,4 +50,7 @@ express()
     }
     res.send(result)
   })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+  module.exports = {
+    server
+};
