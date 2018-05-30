92dac8950c20e5033095a644a62e4e2451e6d40b
Abraham Bueno <abueno@localhost.localdomain>
2018-05-30 00:13:53 -0500

    Fixed some logic in the login

--- api/controllers/loginPath.js
+++ api/controllers/loginPath.js
@@ -3,59 +3,35 @@ const { mysecret } = require('../../config');
 
 const Notes = require('../models/NoteModel');
 
-// const login = (req, res) => {
-//     const { username, password } = req.body;
-//     Notes.findOne({ username }, (err, user) => {
-//         if (err) {
-//             res.status(403).json({ error: 'Invalid username/password, credentials' });
-//             return;
-//         }
-//         if (user === null) {
-//             res.status(422).json({ error: 'No user with that username in notes DB' });
-//             return;
-//         }
-//         user.checkPassword(password, (nonMatch, hashMatches) => {
-//             if (nonMatch !== null) {
-//                 res.status(422).json({ error: 'passwords dont match' });
-//                 return;
-//             }
-//             if (hashMatches) {
-//                 const payload = {
-//                     username: user.username
-//                 };
-//                 const token = jwt.sign(payload, mysecret);
-//                 res.json({ token });
-//             }
-//         });
-//     });
-// };
 const login = (req, res) => {
     const { username, password } = req.body;
     Notes.findOne({ username }, (err, user) => {
-      if (err) {
-        res.status(403).json({ error: 'Invalid Username/Password' });
-        return;
-      }
-      if (user === null) {
-        res.status(422).json({ error: 'No user with that username in our DB' });
-        return;
-      }
-      user.checkPassword(password, (nonMatch, hashMatch) => {
-        // This is an example of using our User.method from our model.
-        if (nonMatch !== null) {
-          res.status(422).json({ error: 'passwords dont match' });
-          return;
+        if (err) {
+            res.status(403).json({ error: 'Invalid username/password, credentials' });
+            return;
         }
-        if (hashMatch) {
-          const payload = {
-            username: user.username
-          }; // what will determine our payload.
-          const token = jwt.sign(payload, mysecret); // creates our JWT with a secret and a payload and a hash.
-          res.json({ token }); // sends the token back to the client
+        if (user === null) {
+            res.status(422).json({ error: 'No user with that username in notes DB' });
+            return;
         }
-      });
+        user.checkPassword(password, (nonMatch, hashMatches) => {
+            console.log('this is the checkpassword:', nonMatch, hashMatches)
+            if (hashMatches === false) {
+                res.status(422).json({ error: 'passwords dont match' });
+                return;
+            }
+            if (hashMatches) {
+
+                const payload = {
+                    username: user.username
+                };
+                const token = jwt.sign(payload, mysecret);
+                res.json({ token });
+            }
+        });
     });
-  };
+};
+
 module.exports = {
     login
 }
