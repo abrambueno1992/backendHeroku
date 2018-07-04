b02cf1d6c33d97035c923ad33dcf9a9509590bd4
Abraham Bueno <abueno@localhost.localdomain>
2018-07-04 14:35:45 -0500

    added a get to index.js file

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,8 +1,10 @@
 const {authenticate} = require('../utils/middleware');
+
 const {createUser, getNote, login, createNotes,  getNoteByID, deleteUser, updateNotes, deleteNote, updateUser} = require('../controllers') //updateNotes, deleteNote,
 // const {createNote} = require('./createPath');
 // const {login} = require('./loginPath');
 // const {getNote} = require('./displayPath.js');
+const pathToIndex = '../../index.js'
 
 module.exports = server => {
     server.get('/api/notes/view', authenticate, getNote);
@@ -12,7 +14,7 @@ module.exports = server => {
     // server.route('/api/notes/view/*').post(authenticate, getNoteByID);
     // server.route('/api/notes/*').post(authenticate, getNoteByID);
     // server.get('/*', authenticate, getNote);
-    server.route('*').post(authenticate, getNoteByID);
+    // server.route('*').post(authenticate, getNoteByID);
     server.route('/api/notes/update').put(authenticate, updateNotes);
     server.route('/api/notes/userupdate').put(authenticate, updateUser);
     server.route('/api/notes/delete').delete(authenticate, deleteNote);
@@ -21,4 +23,11 @@ module.exports = server => {
     server.route('/api/notes/new').post(createUser);
 
     server.route('/api/notes/login').post(login);
+    server.get('/*', function(req, res) {
+        res.sendFile(path.join(__dirname, pathToIndex), function(err) {
+          if (err) {
+            res.status(500).send(err)
+          }
+        })
+      })
 };
